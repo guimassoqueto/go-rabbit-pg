3c5f2b03354df443fdc1db67171ac75727c7ee41
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-24 08:03:01 -0300

    chore: update main

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"grp/rabbitmq"
-)
+import "grp/rabbitmq"
+
 
 func main() {
-	var i = 0
-	for i < 10000 {
-		rabbitmq.Send(fmt.Sprintf("Message #%d", i))
-		i += 1
-	}
 	rabbitmq.Receive()
 }
+
