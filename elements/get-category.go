a118f364089b4686b99273a1be7c21e7c2955eb9
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-24 16:39:04 -0300

    feat: [element] product category

--- elements/get-category.go
+++ elements/get-category.go
@@ -3,12 +3,10 @@ package elements
 import (
 	"regexp"
 	"strings"
-
-	"github.com/gocolly/colly"
 )
 
-func GetCategory(e *colly.HTMLElement) string {
+func GetCategory(eText string) string {
 	regex, _ := regexp.Compile(`[a-zA-Zà-úÀ-Ú0-9_]+`)
-	match := regex.FindAllString(e.Text, -1)
+	match := regex.FindAllString(eText, -1)
 	return strings.Join(match, " ")
 }
